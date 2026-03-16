ba4a5f54dae013b1904161df64efcdb6fccff5a2
Swati Borkar <bswati18000@gmail.com>
2026-03-16 20:01:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1263214660-a30c3",
+  "appId": "1:1008955780282:web:8329d7caae9c33d2952683",
+  "apiKey": "AIzaSyBqP0iiiYu9oWErr3rpNiU5yOBGp2H1CLc",
+  "authDomain": "studio-1263214660-a30c3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1008955780282"
+};
